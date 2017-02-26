e81a36dff07c0deb5627bee16c1a06fd717ba879
Cygnan <mail@cygnan.com>
2017-02-27 05:41:48 +0900

    Updated code.js

--- code.js
+++ code.js
@@ -1,65 +1,15 @@
-function forwardTransitInfo2() {
+﻿		/*
+		 * まずスクリプトプロパティに以下の値を格納しておくこと。
+		 * 'myMobileMail' = 運行情報送信先のメールアドレス
+		 * 'obj.time' = '0'　//値は'0'でなくても構わないが、''はNG。何か値を入れること。
+		 */
+
+function run() {
   try {
-
-    var obj = getServiceStatus();
-    var timeProperty = PropertiesService.getScriptProperties().getProperty('obj.time');
-    //もし最終更新時刻が変わってなかったら終了。もし変わってたらjsonを記録して、メールを送信する。
-    if (obj.time == timeProperty) {
-    	return;
-    } else {
-    	PropertiesService.getScriptProperties().setProperty('obj.time', obj.time);
-    	//とりあえず改めて取得しなおした。
-    	var jsonp = UrlFetchApp.fetch('https://www.seiburailway.jp/api/v1/servicestatus.jsonp')
-    	.getContentText('UTF-8');
-    	var json = jsonp
-    	//最初に「sr_servicestatus_callback(」があったらカット
-    	.replace(/(^sr_servicestatus_callback\()?/, '')
-    	//最初に「sr_emergency_callback(」があったらカット
-   		.replace(/(^sr_emergency_callback\()?/, '')
-    	//最後の「)」をカット
-  		.replace(/\)$/, '');
-    	var myMobileMail = PropertiesService.getScriptProperties().getProperty('mymobileMail');
-    	GmailApp.sendEmail(myMobileMail, '西武鉄道運行情報β', '', {htmlBody:now() + '<br /><br />' + obj.text})
-    	PropertiesService.getScriptProperties().setProperty('TEXT_' + now(), json);
-    }
-    return; //とりあえずデバッグ用に追加した。
-  } catch(e) {
-  	PropertiesService.getScriptProperties().setProperty('ERROR_' + now(), e.toString());
-  }
-}
-
-function now() {
-	var date = new Date();         // 現在日時を生成
-	var mm = date.getMonth() + 1;  // 月を取得（返り値は実際の月-1なので、+1する）
-	var dd = date.getDate(); // 日を取得
-	var w = date.getDay();   // 曜日を取得（数値）
-	var hh = date.getHours();
-	var mi = date.getMinutes();
-	 
-	// 月と日が一桁の場合は先頭に0をつける
-	if (mm < 10) {
-    mm = "0" + mm;
-	}
-	if (dd < 10) {
-    dd = "0" + dd;
-	}
-	if (hh < 10) {
-    hh = "0" + hh;
-	}
-	if (mi < 10) {
-    mi = "0" + mi;
-	}
- 
-	// 曜日を数値から文字列に変換するための配列
-	var week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-	// フォーマットを整えて表示
-	var result = mm + '/' + dd + '[' + week[w] + '] ' + hh + ':' + mi + ' 取得';
-	return result;
-}
-
-function getServiceStatus() {
-	try {
-		var obj = fetchStatusObj('https://www.seiburailway.jp/api/v1/servicestatus.jsonp')
+    var res = UrlFetchApp.fetch('https://www.seiburailway.jp/api/v1/servicestatus.jsonp');
+    if (res.getResponseCode() !== 200) throw new Error('Not Found');
+    var jsonp = res.getContentText('UTF-8');
+		var json = cnvJsonpToJson(jsonp);
 		/*
 		 *　obj.chk					：(謎) ex.0
 		 *	obj.status_code ：ステータスコード　ex.200
@@ -69,59 +19,43 @@ function getServiceStatus() {
 		 *	obj.tif_all			：(振替輸送全区間？)
 		 *	obj.time				：最終更新日時								が入るようにする。
 		 */
-		.IDS2Web[0];
-		return obj;
-	} catch(e) {
-		var errKey = 'ERROR_' + now() + '_' + arguments.callee.name;
-		return PropertiesService.getScriptProperties().setProperty(errKey, e.toString());
-	}
-}
-
-function getEmergencyStatus() {
-	try {
-		var obj2 = fetchStatusObj('https://www.seiburailway.jp/api/v1/emergency.jsonp')
-		/*
-		 *　obj2.chk					：(謎) ex.1
-		 *	obj2.items 				：(謎) ex.null
-		 *	obj2.published		：(謎 何かの文字列) ex.''
-		 *　obj2.status_code	：ステータスコード　ex.200
-		 *	obj2.text					：(何か緊急の情報のテキスト？)
-		 *	obj2.text_twitter	：(何か緊急の情報のテキストのツイッター用短縮版？)
-		 *	obj2.url					：(関連する情報のURL？)								が入るようにする。
-		 */
-		.Emergency[0];
-		return obj2;
-	} catch(e) {
-		var errKey = 'ERROR_' + now() + '_' + arguments.callee.name;
-		return PropertiesService.getScriptProperties().setProperty(errKey, e.toString());
-	}
-}
-
-function fetchStatusObj(url) {
-	/* 
-	 * 引数に指定されたJSONPのURLを取得し、パースしたうえでそのオブジェクトを返します。
-	 * fetchStatusObj( 取得したいホームページのURL/String )
-	 * return parseJsonp(jsonp) によってパースされたもの/Object
-	 */
-  try {
-  	// 因数のURLをurlValidator(url)でURLだと確認する。
-    if (!urlValidator(url)) throw new Error('Invalid args');
-    var res = UrlFetchApp.fetch(url);
-    if (res.getResponseCode() !== 200) throw new Error('Not Found');
-    var jsonp  = res.getContentText('UTF-8');
-		obj = parseJsonp(jsonp);
-	return obj;
+		var obj = JSON.parse(json).IDS2Web[0];
+    var timeFromTheProperty = PropertiesService.getScriptProperties().getProperty('obj.time');
+    //もし最終更新時刻が変わってなかったら終了。もし変わっていたら現在時刻とjsonpを記録して、メールを送信する。
+    if (obj.time == timeFromTheProperty) {
+    	return;
+    } else {
+    	PropertiesService.getScriptProperties().setProperty('obj.time', obj.time);
+    	//運行情報をメールで送信する
+    	emailNotify(obj.text);
+    	//デバッグ用｜JSONPの値をそのままスクリプトプロパティに格納しておく
+    	PropertiesService.getScriptProperties().setProperty('TEXT_' + now(), jsonp);
+    }
+    return; //とりあえずデバッグ用に追加した。
   } catch(e) {
-    var errKey = 'ERROR_' + now() + '_' + arguments.callee.name;
-		return PropertiesService.getScriptProperties().setProperty(errKey, e.toString());
+  	PropertiesService.getScriptProperties().setProperty('ERROR_' + now() + '_' + arguments.callee.name, e.toString());
   }
 }
 
-function parseJsonp(jsonp) {
+function emailNotify(status) {
 	/*
-	 * JSONPで書かれた文字列をJSONに変換したうえで、Objectにパースする。
-	 * parseJsonp( JSONP/String )
-	 * return パース後/Object
+	 * 運行情報をメールで送信する
+	 * emailNotify( 運行情報のテキストつまりobj.text/String )
+	 * return なし
+	 */
+	 	try {
+		var myMobileMail = PropertiesService.getScriptProperties().getProperty('myMobileMail');
+		GmailApp.sendEmail(myMobileMail, '西武鉄道運行情報β', '', {htmlBody: now() + ' 取得<br /><br />' + status});
+	} catch(e) {
+		PropertiesService.getScriptProperties().setProperty('ERROR_' + now() + '_' + arguments.callee.name, e.toString());
+	}
+}
+
+function cnvJsonpToJson(jsonp) {
+	/*
+	 * JSONPで書かれた文字列をJSONに変換する。Objectにパースはしてくれないように変更。
+	 * cnvJsonpToJson( JSONP/String )
+	 * return Json/String
 	 */
   try {
   	var json = jsonp
@@ -131,19 +65,58 @@ function parseJsonp(jsonp) {
     .replace(/(^sr_emergency_callback\()?/, '')
     //最後の「)」をカット
     .replace(/\)$/, '');
-    var obj = JSON.parse(json);
-    return obj;
+    return json;
   } catch(e) {
-  	var errKey = 'ERROR_' + now() + '_' + arguments.callee.name;
-		return PropertiesService.getScriptProperties().setProperty(errKey, e.toString());
+  	var errorKey = 'ERROR_' + now() + '_' + arguments.callee.name;
+		return PropertiesService.getScriptProperties().setProperty(errorKey, e.toString());
   }
 }
 
-function urlValidator(str) {
-	/* 引数に指定された文字列がURLであったらTrueを返します。
-	 * str/String
-	 * return strがURLかどうか/Booleen
-	 */
-  var regexp = /^(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$/;
-  return (typeof str == 'string' && regexp.test(str));
+function now() {
+	var date = new Date();  // 現在日時を生成
+	var MM = date.getMonth() + 1;  // 月を取得（返り値は実際の月-1なので、+1する）
+	var dd = date.getDate();  // 日を取得
+	var hh = date.getHours();  // 時を取得
+	var mm = date.getMinutes();  // 分を取得
+	var w = date.getDay();  // 曜日を取得（数値）
+	// 月、日、時、分が一桁の場合は先頭に0をつける
+	if (MM < 10) {
+    MM = "0" + MM;
+	}
+	if (dd < 10) {
+    dd = "0" + dd;
+	}
+	if (hh < 10) {
+    hh = "0" + hh;
+	}
+	if (mm < 10) {
+    mm = "0" + mm;
+	}
+	// 曜日を数値から文字列に変換するための配列
+	var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+	// フォーマットを整える
+	var result = MM + '/' + dd + '[' + week[w] + '] ' + hh + ':' + mm;
+	return result;
 }
+
+/*
+function getEmergencyStatus() {
+	try {
+		var obj2 = fetchStatusObj('https://www.seiburailway.jp/api/v1/emergency.jsonp')
+		 *
+		 *　obj2.chk					：(謎) ex.1
+		 *	obj2.items 				：(謎) ex.null
+		 *	obj2.published		：(謎 何かの文字列) ex.''
+		 *　obj2.status_code	：ステータスコード　ex.200
+		 *	obj2.text					：(何か緊急の情報のテキスト？)
+		 *	obj2.text_twitter	：(何か緊急の情報のテキストのツイッター用短縮版？)
+		 *	obj2.url					：(関連する情報のURL？)								が入るようにする。
+		 *
+		.Emergency[0];
+		return obj2;
+	} catch(e) {
+		var errorKey = 'ERROR_' + now() + '_' + arguments.callee.name;
+		return PropertiesService.getScriptProperties().setProperty(errorKey, e.toString());
+	}
+}
+*/
